f3e42da62f714446f5a6ab117349d82524c1b9ef
Adrian <golinski.adrian@gmail.com>
2021-09-06 12:20:02 +0200

    fixes bug

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
     QString styleSheet { QLatin1String(file.readAll()) };
 
     //setup stylesheet
-    w.setStyleSheet(styleSheet);
+    a.setStyleSheet(styleSheet);
     w.show();
     return a.exec();
 }
